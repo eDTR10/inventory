3728dbb332a922969024ee5c261a0f517e323fb5
iannico322 <iannicocaulin@gmail.com>
2026-02-10 11:13:41 +0800

    Updates

---
+++ assets/chevron-left-e9988d65.js
@@ -0,0 +1,1 @@
+import{c as e}from"./index-569f3a7b.js";const t=e("ChevronLeft",[["path",{d:"m15 18-6-6 6-6",key:"1wnfg3"}]]);export{t as C};
---
+++ assets/chevron-right-ec143e7c.js
@@ -0,0 +1,1 @@
+import{c as t}from"./index-569f3a7b.js";const h=t("ChevronRight",[["path",{d:"m9 18 6-6-6-6",key:"mthhwq"}]]);export{h as C};
